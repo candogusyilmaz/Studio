88310998a509abc06f9759a204ac5f04c01c5166
Can <candogusyilmaz@gmail.com>
2022-12-30 19:36:41 +0300

    Added handler to handle validation exception.

--- studio-api/src/main/java/cdy/studioapi/exceptions/ExceptionHandlerAdvice.java
+++ studio-api/src/main/java/cdy/studioapi/exceptions/ExceptionHandlerAdvice.java
@@ -1,8 +1,15 @@
 package cdy.studioapi.exceptions;
 
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+import java.util.HashMap;
+import java.util.Map;
 
 @RestControllerAdvice
 public class ExceptionHandlerAdvice {
@@ -10,5 +17,17 @@ public class ExceptionHandlerAdvice {
     @ExceptionHandler(ApiException.class)
     public ResponseEntity<ErrorResponse> handleApiException(ApiException exception) {
         return new ResponseEntity<>(exception.errorResponse, exception.httpStatus);
+    }
+
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public Map<String, String> handleValidationExceptions(MethodArgumentNotValidException ex) {
+        Map<String, String> errors = new HashMap<>();
+        ex.getBindingResult().getAllErrors().forEach(error -> {
+            String fieldName = ((FieldError) error).getField();
+            String errorMessage = error.getDefaultMessage();
+            errors.put(fieldName, errorMessage);
+        });
+        return errors;
     }
 }
